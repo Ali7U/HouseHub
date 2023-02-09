06cafd8caf7f8a0116bb7a20478f8bdd2e79f843
Ali <alipro.ig7@gmail.com>
2023-02-09 21:32:33 +0300

    add chakra ui and router with 3 files

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./Footer";
+import Nav from "./Nav";
 
 function Router() {
   return (
     <div>
-      
+      <Nav />
+      <Routes>{/* <Route path="/" element={<Nav />} /> */}</Routes>
+      <Footer/>
     </div>
-  )
+  );
 }
 
-export default Router
+export default Router;
