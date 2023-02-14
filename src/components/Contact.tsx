36d9bf0fb63bcc49fe385990992e2b189e10f899
TURKI-20 <turki-fahad1996@hotmail.com>
2023-02-14 10:33:54 +0300

    add in Contact (subject) and CSS

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
-// import { FaUserAlt } from "react-icons/fa";
-// import { BsTelephoneFill } from "react-icons/Bs";
-// import {MdEmail} from "react-icons/Md"
-// import {BsChatRightTextFill} from "react-icons/Bs"
+import { FaUserAlt } from "react-icons/fa";
+import { BsTelephoneFill } from "react-icons/Bs";
+import {MdEmail} from "react-icons/Md"
+import {BsChatRightTextFill} from "react-icons/Bs"
+import {MdSubtitles} from "react-icons/Md"
 import './Contact.css'
 export default function Contact() {
   return (
@@ -17,20 +18,24 @@ export default function Contact() {
         
             <div className="input-group">
             <input type="text" id="name" required />
-            <label htmlFor="name"> Your name</label>
+            <label htmlFor="name"><FaUserAlt/> Your name</label>
          </div>
          <div className="input-group">
             <input type="text" id="number" required />
-            <label htmlFor="number" >Phone number</label>
+            <label htmlFor="number" ><BsTelephoneFill/> Phone number</label>
          </div>
         </div>
          <div className="input-group">
             <input type="email" id="email" required />
-            <label htmlFor="email"> Your Email</label>
+            <label htmlFor="email"><MdEmail/> Your Email</label>
+         </div>
+         <div className="input-group">
+            <input type="text" id="subject" required />
+            <label htmlFor="subject"><MdSubtitles/> Subject</label>
          </div>
          <div className="input-group">
             <textarea id="message" rows={8} required></textarea>
-            <label className='z8' htmlFor='message'> Your Message</label>
+            <label className='z8' htmlFor='message'><BsChatRightTextFill/> Your Message</label>
          </div>
          <button type="submit" id="btn1"> SEND</button>
     </form>
