57c6863c9427c0dde546e741918aa2798b1252cc
Riyan Almaiman <ralmaiman@gmail.com>
2023-02-15 12:27:15 +0300

    turki's page

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -37,7 +37,11 @@ function Main() {
     setFilterArea(e.target.value ? parseInt(e.target.value) : null);
   };
 
-  const [isOpen, onToggle] = React.useState<any>();
+
+
+  const [ isOpen, onToggle ] = React.useState<any>();
+
+
 
   interface houses {
     imageUrl: string;
@@ -78,18 +82,7 @@ function Main() {
       imageAlt: "Rear view of modern home with pool",
       Area: 500,
       Rooms: 1,
-
-      Floors: 2,
-    },
-    {
-      imageUrl:
-        "https://media.istockphoto.com/id/864458934/photo/architects-engineer-discussing-at-the-table-with-blueprint-closeup-on-hands-and-project-print.jpg?s=612x612&w=0&k=20&c=-ODOOfKY-vxwhyjx7RggEvW3YzWrinoELglqZAbjzIs=",
-      imageAlt: "Rear view of modern home with pool",
-     
-
-      Area: 200,
-      Rooms: 1,
-      Floors: 2,
+      Floors: 1,
     },
 // --------------------500---------------
 {
@@ -207,15 +200,15 @@ function Main() {
     
   ];
 
+    
   return (
     <>
-
       <Center m={35}>
         <Stack direction="row" spacing={8} align="center"  >
           
           <Button
 
-
+            onClick={() => {AreaButtons(200); onToggle(true)}}
 
             colorScheme='blue'
             variant="outline" 
@@ -226,9 +219,8 @@ function Main() {
           </Button>
           <Button
 
-
-onClick={() => {AreaButtons(300); onToggle(true)}}
-colorScheme="teal"
+            onClick={() => {AreaButtons(300); onToggle(true)}}
+            colorScheme='blue'
             variant="outline"
             h={61}
             fontSize={21}
@@ -236,10 +228,9 @@ colorScheme="teal"
             300m<sup>2</sup>
           </Button>
           <Button
-
-
-onClick={() => {AreaButtons(400); onToggle(true)}}
-            colorScheme="teal"
+          
+            onClick={() => {AreaButtons(400); onToggle(true)}}
+            colorScheme='blue'
             variant="outline"
             h={61}
             fontSize={21}
@@ -248,19 +239,21 @@ onClick={() => {AreaButtons(400); onToggle(true)}}
           </Button>
           <Button
 
-
             onClick={() => {AreaButtons(500); onToggle(true)}}
             colorScheme='blue'
             variant="outline"
             h={61}
             fontSize={21}
           >
+
             500m<sup>2</sup>
           </Button>
         </Stack>
       </Center>
 
       <Center
+
+       
         backgroundSize={"cover"}
         display="flex"
         justifyContent={"space-around"}
@@ -268,7 +261,6 @@ onClick={() => {AreaButtons(400); onToggle(true)}}
         w={"100%"}
         
       >
-
         {property.filter(item=>item.Area==filterArea).map((item) => (
 
           <Container>
@@ -278,61 +270,12 @@ onClick={() => {AreaButtons(400); onToggle(true)}}
                   borderWidth="1px"
                   borderRadius="lg"
                   overflow="hidden"
+                  backgroundColor={"#FFFDF1"}
+                  h="300px"
+                >
+                  <Image src={item.imageUrl} alt={item.imageAlt} transition={''}/>
 
-                  backgroundColor={"#ccc8"}
-                  >
-                  <Image src={item.imageUrl} alt={item.imageAlt} />
-
-                  <Box p="6">
-                    <Box display="flex" alignItems="baseline">
-                      <Badge borderRadius="full" px="2" colorScheme="teal">
-                        New
-                      </Badge>
-                      <Box
-                        color="gray.500"
-                        fontWeight="semibold"
-                        letterSpacing="wide"
-                        fontSize="xs"
-                        textTransform="uppercase"
-                        ml="2"
-                        >
-                        {/* {item.beds} beds &bull; {item.baths} baths */}
-                      </Box>
-                    </Box>
-
-                    <Box
-                      mt="1"
-                      fontWeight="semibold"
-                      as="h4"
-                      lineHeight="tight"
-                      noOfLines={1}
-                      >
-                      {/* {item.title} */}
-                    </Box>
-
-                    <Box>
-                      {/* {item.formattedPrice} */}
-                      <Box as="span" color="gray.600" fontSize="sm">
-                        / wk
-                      </Box>
-                    </Box>
-
-                    <Box display="flex" mt="2" alignItems="center">
-                      {Array(5)
-                        .fill("")
-                        .map((_, i) => (
-                          <StarIcon
-                          key={i}
-                          // color={i < item.rating ? "teal.500" : "gray.300"}
-                          />
-                          ))}
-                      <Box as="span" ml="2" color="gray.600" fontSize="sm">
-                        {/* {item.reviewCount} reviews */}
-                      </Box>
-                    </Box>
-                  </Box>
                 </Box>
-
                     <Box>
                     <Stack direction="row" paddingTop={5} paddingLeft={120}>
                       <Button backgroundColor={'#e6d894'} onClick={()=>{}}>Select</Button>
@@ -340,11 +283,9 @@ onClick={() => {AreaButtons(400); onToggle(true)}}
         </Collapse>
           </Container>
 
-))}
+        ))}
       </Center>
-
-
-
+      
     </>
   );
 }
