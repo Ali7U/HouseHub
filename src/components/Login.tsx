d6a0246b49cb146b0025c3d79193edd7e489e492
Riyan <109624643+Riyan-Almaiman@users.noreply.github.com>
2023-02-18 15:25:50 +0300

    updated incorrect api link

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,6 +9,7 @@ function Login() {
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
+  const [Id, setID] = useState('');
 
   useEffect(() => {
     const storedUserName = localStorage.getItem('userName');
@@ -17,24 +18,22 @@ function Login() {
     }
   }, []);
 
-  const handleLogin = async () => {
+  const handleLogin = async () => { console.log(password); console.log(userName)
     setError('');
-    if (!userName || !password) {
-      setError('Please enter both username and password');
-      return;
-    }
+    
     try {
-      const response = await axios.get(`https://63e20921ad0093bf29c66077.mockapi.io/Signup?username=${userName}`);
-      if (response.data.length === 0) {
-        setError('Incorrect username or password');
-        return;
-      }
+      const response = await axios.get(`https://63e208d4ad0093bf29c65b2d.mockapi.io/Users/?userName=${userName}`);
+     
       const user = response.data[0];
+      console.log(user);
+
       if (user.password !== password) {
         setError('Incorrect username or password');
         return;
       }
       localStorage.setItem('userName', userName);
+      localStorage.setItem('id', user.id);
+
       window.location.href = '/home';
     } catch (err) {
       setError('An error occurred, please try again later');
